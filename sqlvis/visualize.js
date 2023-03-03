3d8aef2318670e274702396bce2f85a9254f0cfc
Pacmega <bas.witters@hotmail.com>
2023-03-03 19:22:28 +0100

    ALL MY ADDITIONS CURRENTLY BROKEN - Added TODOs   and started upgrading of attemptOrderingFix (and yes, I realise you are not supposed to commit broken code)

--- sqlvis/visualize.js
+++ sqlvis/visualize.js
@@ -43696,37 +43696,152 @@ THE SOFTWARE.
                               });
 
 
-function determineKeywordOrder(query, keywordsToFind) {
-  var keywordArray = [];
-  var indexArray = [];
+function allIndicesOf(searchString, matchString) {
+  let matches = [];
 
-  for (var index in keywordsToFind) {
-    var keyword = keywordsToFind[index];
-    var foundAtIndex = query.indexOf(keyword);
+  let matchAtIndex = searchString.indexOf(matchString);
+  while (matchAtIndex != -1) {
+    // A match was found, store its location and continue checking
+    //   from 1 character further forward for other matches
+    matches.push(matchAtIndex);
+    matchAtIndex = searchString.indexOf(matchString, matchAtIndex + 1);
+  }
 
-    if (foundAtIndex !== -1) {
-      // Keyword exists in query, appears at char index foundAtIndex
+  return matches;
+}
+
+function findKeywordAppearances(query, keywordsToFind) {
+  let keywordArray = [];
+  let indexArray = [];
+
+  for (let index in keywordsToFind) {
+    let keyword = keywordsToFind[index];
+    let foundAtIndices = allIndicesOf(query, keyword);
+
+    if (foundAtIndices.length > 0) {
+      // Keyword exists at least once in query
       keywordArray.push(keyword);
-      indexArray.push(foundAtIndex);
+      indexArray.push(foundAtIndices);
     }
   }
 
   return {foundKeywords: keywordArray, foundIndices: indexArray}
 }
 
+function findThisLevelEnd(query) {
+  // TODO: currently unused function, also working on its calling func.
+
+  // Receives: a subset of a query, cut after at least one opening bracket.
+  // Returns: the index within this subset where the closing bracket is found.
+
+  // TODO: untested function! Need to test this.
+  const bracketOpenedAt = query.indexOf('(');
+  let bracketClosedAt = query.indexOf(')');
+
+  if (bracketClosedAt === -1) {
+    // There is no closing bracket, this level does not appear to end.
+    return -1;
+  }
+  
+  if (bracketOpenedAt !== -1 && bracketOpenedAt < bracketClosedAt) {
+    // At least one extra set of brackets is opened and closed within
+    //   this level. Our closing bracket must occur after that.
+    let endOfBracketSet = findThisLevelEnd(query.slice(bracketOpenedAt + 1));
+    if (endOfBracketSet === -1) {
+      // The deeper layer of brackets is never closed. That means this level 
+      //   cannot possibly be closed either, and this query has a problem.
+      return -1;
+    }
+    // We know where that set of brackets ends, now continue looking from
+    //   past that end to find the closing bracket.
+    bracketClosedAt = findThisLevelEnd(query.slice(endOfBracketSet + 1));
+  }
+  
+  // If/when this point is reached (at any level of recursion), either a
+  //   closing bracket was found without an opening bracket in front of it,
+  //   which means this closing bracket ends this query level, or no closing
+  //   bracket was found for this level at all which means we found a problem.
+  return bracketClosedAt;
+}
+
+
+function subsetQueryToLevelAtIndex(query, index) {
+  // Find the level of this index
+  const queryUpToIndex = query.slice(0, index);
+  const openingBracketsBefore = allIndicesOf(queryUpToIndex, '(')
+  const closingBracketsBefore = allIndicesOf(queryUpToIndex, ')')
+
+  const queryFromIndex = query.slice(index);
+  const openingBracketsAfter = allIndicesOf(queryFromIndex, '(')
+  const closingBracketsAfter = allIndicesOf(queryFromIndex, ')')
+
+  // If the start of queryFromIndex is not the opening bracket we are trying
+  //   to find the closing bracket for, behavior is slightly different.
+  const charAtIndex = query[index];
+  const levelStartsAtIndex = (charAtIndex === '(' ? true : false);
+
+  const currentLevel = openingBracketsBefore.length - closingBracketsBefore.length;
+
+  // Context checking for query subsetting
+  if (currentLevel === 0 && !levelStartsAtIndex) {
+    // We are at the top level here. There is no subsetting to perform.
+    return query;
+  }
+
+  let locationOfOpeningBracket;
+  // Find the start of this level, so that we can slice from there.
+  if (levelStartsAtIndex) {
+    locationOfOpeningBracket = index;
+  } else {
+    locationOfOpeningBracket = queryUpToIndex.lastIndexOf('(');
+  }
+  
+  // Find the end of this level, so that we can slice until there.
+  const bracketEndingLevelIndex = closingBracketsAfter.length - openingBracketsAfter.length
+                                  - Number(levelStartsAtIndex);
+  
+  let locationOfClosingBracket = closingBracketsAfter[bracketEndingLevelIndex];
+  if (typeof locationOfClosingBracket === 'undefined') {
+    throw Error('No closing bracket found for query level subsetting of query for level at '
+                + 'char index ' + index + '! Query: \n' + query);
+  }
+
+  // Since this location is based on a string slice, add the number of chars
+  //   that were before the slice to find the index in the original string.
+  locationOfClosingBracket += index;
+  
+  // const endOfThisLevel = locationOfOpeningBracket + findLevelEnd(query.slice(index));
+
+  // Slice is inclusive at the start, +1 to avoid bringing the opening bracket.
+  // It is exclusive at the end, so the closing bracket is left out automatically.
+  const subquery = query.slice(locationOfOpeningBracket+1, locationOfClosingBracket);
+  return subquery
+}
+
 function attemptOrderingFix(query) {
+  // High prio:
+  // TODO: multiple appearances of the same keyword in the same level
+  //         is not handled well.
+  // TODO: Subquery handling
+  // TODO: WHERE -> GROUPBY if uses agg and groupby present
+  // TODO: find & handle KEYWORD GROUP BY situations
+  // TODO: find & handle FUNCTION(GROUP BY column) situations
+
+  // Lower prio:
   // TODO: intentionally neglecting UNION for now, because it
   //   makes things more complicated.
   // TODO: need to add the different join types in a way that just 'join'
   //   doesn't trigger on every single one.
-  // TODO: multiple appearances of the same keyword is not handled well.
   
   // This is also the order in which the keywords should appear.
   const keywordsToFind = ['with', 'select', 'from', 'join', 'on',
                           'where', 'group by', 'having', 'order by'];
 
   var lowercaseQuery = query.toLowerCase();
-  var keywordStatus = determineKeywordOrder(lowercaseQuery, keywordsToFind);
+  // TODO: this used to return key:val with val=first occurrance.
+  //   val however is now an array of all appearances.
+  // This also means sorting doesn't make sense anymore.
+  var keywordStatus = findKeywordAppearances(lowercaseQuery, keywordsToFind);
 
   var sortedIndices = Array.from(keywordStatus.foundIndices).sort((a, b) => {return a - b});
 
@@ -45895,12 +46010,16 @@ define(function() {
 
   var e = {};
 
-  e.returnsAAAAAA = function() {
-    return 'AAAAAA';
+  e.findThisLevelEnd = function(query) {
+    return findThisLevelEnd(query);
   }
 
   e.attemptOrderingFix = function(query) {
     return attemptOrderingFix(query);
+  }
+
+  e.subsetQueryToLevelAtIndex = function(query, index) {
+    return subsetQueryToLevelAtIndex(query, index);
   }
 
   e.queryTextAdjustments = function(query) {
